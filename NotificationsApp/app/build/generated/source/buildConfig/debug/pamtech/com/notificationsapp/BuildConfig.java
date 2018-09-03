9e4ebf77517d9926c675f11744a7011780023647
pascal-ik <42011849+pascal-ik@users.noreply.github.com>
2018-09-03 17:12:17 -0400

    Initial build

---
+++ NotificationsApp/app/build/generated/source/buildConfig/debug/pamtech/com/notificationsapp/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package pamtech.com.notificationsapp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "pamtech.com.notificationsapp";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
