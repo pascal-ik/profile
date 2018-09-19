e802c195e4ca8f0cb6b0a22c40059f3b1b124a3a
pascal-ik <42011849+pascal-ik@users.noreply.github.com>
2018-09-19 17:21:55 -0400

    Add files via upload

--- WeatherApi/app/build/generated/source/r/debug/android/support/constraint/R.java
+++ WeatherApi/app/build/generated/source/r/debug/android/support/constraint/R.java
@@ -69,18 +69,18 @@ public final class R {
     public static final class id {
         public static final int bottom = 0x7f070021;
         public static final int end = 0x7f070036;
-        public static final int gone = 0x7f07003e;
-        public static final int invisible = 0x7f070047;
-        public static final int left = 0x7f07004a;
-        public static final int packed = 0x7f070058;
-        public static final int parent = 0x7f070059;
-        public static final int percent = 0x7f07005b;
+        public static final int gone = 0x7f07003d;
+        public static final int invisible = 0x7f070046;
+        public static final int left = 0x7f070049;
+        public static final int packed = 0x7f070057;
+        public static final int parent = 0x7f070058;
+        public static final int percent = 0x7f07005a;
         public static final int right = 0x7f07005f;
-        public static final int spread = 0x7f070077;
-        public static final int spread_inside = 0x7f070078;
-        public static final int start = 0x7f07007d;
-        public static final int top = 0x7f07008a;
-        public static final int wrap = 0x7f070095;
+        public static final int spread = 0x7f070078;
+        public static final int spread_inside = 0x7f070079;
+        public static final int start = 0x7f07007e;
+        public static final int top = 0x7f07008c;
+        public static final int wrap = 0x7f070094;
     }
     public static final class styleable {
         public static final int[] ConstraintLayout_Layout = { 0x010100c4, 0x0101011f, 0x01010120, 0x0101013f, 0x01010140, 0x7f020037, 0x7f020038, 0x7f02004d, 0x7f020060, 0x7f020061, 0x7f0200a8, 0x7f0200a9, 0x7f0200aa, 0x7f0200ab, 0x7f0200ac, 0x7f0200ad, 0x7f0200ae, 0x7f0200af, 0x7f0200b0, 0x7f0200b1, 0x7f0200b2, 0x7f0200b3, 0x7f0200b4, 0x7f0200b5, 0x7f0200b6, 0x7f0200b7, 0x7f0200b8, 0x7f0200b9, 0x7f0200ba, 0x7f0200bb, 0x7f0200bc, 0x7f0200bd, 0x7f0200be, 0x7f0200bf, 0x7f0200c0, 0x7f0200c1, 0x7f0200c2, 0x7f0200c3, 0x7f0200c4, 0x7f0200c5, 0x7f0200c6, 0x7f0200c7, 0x7f0200c8, 0x7f0200c9, 0x7f0200ca, 0x7f0200cb, 0x7f0200cc, 0x7f0200cd, 0x7f0200ce, 0x7f0200cf, 0x7f0200d0, 0x7f0200d2, 0x7f0200d3, 0x7f0200d4, 0x7f0200d5, 0x7f0200d6, 0x7f0200d7, 0x7f0200d8, 0x7f0200d9, 0x7f0200dc };
